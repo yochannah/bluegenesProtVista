47c5ed75d0e7f4fa920515352c4138e06975e8c5
Yo Yehudi <yochannah@gmail.com>
2017-11-02 15:12:58 +0000

    scaffold tool out

---
+++ src/index.js
@@ -0,0 +1,9 @@
+var bluegenesProtvista = function (el, service, package, state) {
+
+    var ProtVista = require('ProtVista');
+
+    var instance = new ProtVista({
+        el: el,
+        uniprotacc: 'P05067'
+    });
+}
