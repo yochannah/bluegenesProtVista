3b79a8f1b02010fb4f22928f40cffb40575d80f1
Yo Yehudi <yochannah@gmail.com>
2017-11-27 14:39:21 +0000

    set up with config to select correct column type for the tool

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-var bluegenesProtvista = function (el, service, package, state) {
-
-    var ProtVista = require('ProtVista');
-
-    var instance = new ProtVista({
-        el: el,
-        uniprotacc: 'P05067'
+var bluegenesProtvista = function (el, service, package, state, config) {
+    //protVista expects an accession, so convert intermine id to accession
+    //TODO: How do we handle multiple values, e.g. list ids? 
+    var columnToConvert = config.columnMapping[package.class][package.format];
+    var accession = new imjs.Service(service)
+        .findById(package.class, package.value)
+        .then(function(response) {
+            var ProtVista = require('ProtVista');
+            var instance = new ProtVista({
+                el: el,
+                uniprotacc: response[columnToConvert]
+        });
     });
 }
 
