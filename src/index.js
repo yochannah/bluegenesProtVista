fcdde43150cb895d77a1e313ad6d1e867797f399
Yo Yehudi <yochannah@gmail.com>
2018-05-08 12:42:12 +0100

    update repo to bundle with webpack and fix example to work with this too

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-var bluegenesProtvista = function (el, service, package, state, config) {
+import ProtVista from "ProtVista";
+
+export function main (el, service, imEntity, state, config) {
     //protVista expects an accession, so convert intermine id to accession
-    //TODO: How do we handle multiple values, e.g. list ids? 
-    var columnToConvert = config.columnMapping[package.class][package.format];
+    //TODO: How do we handle multiple values, e.g. list ids?
+    var columnToConvert = config.columnMapping[imEntity.class][imEntity.format];
     var accession = new imjs.Service(service)
-        .findById(package.class, package.value)
+        .findById(imEntity.class, imEntity.value)
         .then(function(response) {
-            var ProtVista = require('ProtVista');
+//            var ProtVista = require('ProtVista');
             var instance = new ProtVista({
                 el: el,
                 uniprotacc: response[columnToConvert]
         });
     });
 }
-
+//export default bluegenesProtvista;
